f5a912b30269648d126f4f6382a3c51b3aa037d5
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-11-26 15:30:56 -0500

    create a toast for notification

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -61,9 +62,9 @@ function CabinRow({ cabin }) {
       queryClient.invalidateQueries({
         queryKey: ['cabins'],
       });
-      alert("Cabin successfully deleted")
+      toast.success('Cabin successfully deleted');
     },
-    onError: (err) => alert(err.message), //This function receives the error that may be thrown by the deleteCabin() function
+    onError: (err) => toast.error(err.message), //This function receives the error that may be thrown by the deleteCabin() function
   });
 
   return (
