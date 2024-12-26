44507b84465629976f620a7078e68a186954b8fd
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-12-26 18:17:06 -0500

    load recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useRecentBookings } from './useRecentBookings';
+import Spinner from '../../ui/Spinner';
+import { useRecentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,20 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
+  const {isLoading: isLoadingStays, stays, confirmedStays} = useRecentStays()
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
